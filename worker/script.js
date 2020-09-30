bf54c63d6b793f133311cd75825b381df0b1310b
Pryimak-Volodya <kikimora11%08>
2020-09-30 17:52:05 +0300

    Worker

--- worker/script.js
+++ worker/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     var golovna = "../index.html";
 
     $('#exit').on('click', function () {
-        $(location).attr('href',golovna);
+        $(location).attr('href', golovna);
     });
+    $('.addDeal').on('click', function () {
+        $('.edit ol').append('<li><input></li>');
+        $('.edit ol li').append('')
+    })
 });
