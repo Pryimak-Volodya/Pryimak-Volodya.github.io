791dec762f848f4af4805a4c9b659c83b3ef70f8
AndiMelnikk <amelnik464@gmail.com>
2020-09-30 12:53:23 +0300

    andirri

--- admin/script.js
+++ admin/script.js
@@ -1,10 +1,23 @@
+window.onload = function () {
+    clonv = $('ul').clone();
+    $('.ul_user')[0].children[0].remove()
+};
 $($('.edit_user_menu_2')[0]).children().on('click', user_list);
 function user_list() {
+    $.ajax({
+        url: '../user.json',
+        method: 'get',
+        async: false,
+        dataType: 'html',
+        success: function (name) {
+            person = JSON.parse(name);
+        }
+    });
     switch (this.innerHTML) {
-        case 'Всі працівники':
+        case 'All worker':
             all_user()
             break
-        case 'Адміністратори':
+        case 'Admin':
             andin_user()
             break
         case 'Модератори':
@@ -15,18 +28,8 @@ function user_list() {
             break
     }
 };
-let clonv = $('ul').clone();
-$('.ul_user')[0].children[0].remove()
+
 function all_user() {
-    $.ajax({
-        url: '../user.json',
-        method: 'get',
-        async: false,
-        dataType: 'html',
-        success: function (name) {
-            person = JSON.parse(name);
-        }
-    });
     for (i = 0; i < person.length; i++) {
         $(clonv).clone().appendTo($('.ul_user'));
         list = Array.from($('.ul_user')[0].children[i].children[0].children);
@@ -34,5 +37,15 @@ function all_user() {
         list[1].children[1].innerHTML = person[i].name;
         list[2].children[1].innerHTML = person[i].password;
         list[3].children[1].innerHTML = person[i].status;
+        if (person[i].status == 'admin') {
+            list[4].remove()
+            list[5].remove()
+        }
+    }
+}
+function andin_user() {
+    for (i = 0; i < person.length; i++) {
+        if (person[i].status == 'admin') {
+        }
     }
 }
